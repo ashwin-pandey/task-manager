d328491f1f9c5b836be554764c85aeb60850c618
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2021-09-02 18:49:31 +0530

    create/get list/task completed

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { List } from 'src/app/models/list.model';
+import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-task-view',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskViewComponent implements OnInit {
 
-  constructor() { }
+  lists: List[];
+  tasks: Task[];
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.taskService.getTasks(params.listId).subscribe((tasks: any) => {
+        this.tasks = tasks;
+      });
+    });
+
+    this.taskService.getLists().subscribe((lists: any) => {
+      this.lists = lists;
+    });
+  }
+
+  onTaskClick(task: Task) {
+    // set the task to completed
+    this.taskService.complete(task).subscribe(() => {
+      task.completed = !task.completed;
+    });
   }
 
 }
